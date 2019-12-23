ee9c36bd212ba35c6c6a8cb4787c8b5889271143
elvin <18001268330@163.com>
2019-12-23 23:19:14 +0800

    fix import

--- buukle-activiti-web/src/main/java/top/buukle/activiti/web/ActivitiApplication.java
+++ buukle-activiti-web/src/main/java/top/buukle/activiti/web/ActivitiApplication.java
@@ -1,9 +1,7 @@
 package top.buukle.activiti.web;
 
-import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
